dc67b6bc8f522a23fe4ee1bfb89ca7abece3aa54
Bruno Duarte <duartebruno2208@gmail.com>
2023-02-07 19:48:45 -0300

    feat: header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css'
+
+export function Header() {
+  return (
+    <header>
+      <h1 className={styles.title}>Header</h1>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@/components/Header/Header'
 export default function Home() {
-  return <h1>hello</h1>
+  return <Header />
 }
