098bb5f09ba9dffa3cb2d9b2ad82c01d1b704473
Bruno Duarte <duartebruno2208@gmail.com>
2023-02-07 21:32:47 -0300

    refactor: deleted unused files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
+import { Inter } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style
+        jsx
+        global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
