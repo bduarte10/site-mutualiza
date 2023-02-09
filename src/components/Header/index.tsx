480f2820763db9763e01016841ddad8b60cd6a75
Bruno Duarte <duartebruno2208@gmail.com>
2023-02-09 12:40:55 -0300

    feat: adiciona renderizacao condicional de menu responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Desktop } from './Desktop'
 import styles from './header.module.css'
+import { Mobile } from './Mobile'
 
 export function Header() {
   const [activePage, setActivePage] = useState<string>('home')
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    setWidth(window.innerWidth)
+    window.addEventListener('resize', () => setWidth(window.innerWidth))
+  }, [])
   return (
     <header className={styles.header}>
       <div className="container">
@@ -11,10 +18,14 @@ export function Header() {
           <div>
             <h1>Logo</h1>
           </div>
-          <Desktop
-            activePage={activePage}
-            setActivePage={setActivePage}
-          />
+          {width >= 720 ? (
+            <Desktop
+              activePage={activePage}
+              setActivePage={setActivePage}
+            />
+          ) : (
+            <Mobile />
+          )}
         </div>
       </div>
     </header>
