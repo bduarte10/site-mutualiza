70334eba3558018aef5cef6475ffb1bc6d50c7ac
Bruno Duarte <duartebruno2208@gmail.com>
2023-02-09 17:33:07 -0300

    refactor: hero content

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,23 +1,34 @@
 import styles from './hero.module.css'
 
 export function Hero() {
+  return (
+    <section className={styles.hero_bg}>
+      <div className="container">
+        <div className={styles.content}>
+          <div className={styles.content_items}>
+            <div>
+              <h1>
+                Resolvemos suas demandas ambientais com resultados efetivos.
+              </h1>
+              <h3>
+                We offer a professional advice, guidance and actionable
+                solutions to businesses experiencing issues they can’t deal with
+                in-house.
+              </h3>
 
-    return(
-        <section className={styles.hero_bg}>
-            <div className="container">
-                <div className={styles.content}>
-                    <div className={styles.content_items}>
-                        <div>
-                            <h1>Resolvemos <br /> Suas demandas <br /> Ambientais<br /> Com resultados <br /> Efetivos.</h1>
-
-                            <button>Fale conosco</button>
-                        </div>
-                    </div>
-                    <div className={styles.content_img}>
-                        <div><img src="" alt="" /></div>
-                    </div>
-                </div>
+              <button>Fale conosco</button>
             </div>
-        </section>
-    )
+          </div>
+          <div className={styles.content_img}>
+            <div>
+              <img
+                src=""
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
