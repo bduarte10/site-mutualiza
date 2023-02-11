95ea4c227b6e24450bda8f70b237c6a2ba51b2ca
israelsanttana <israelsantanasantos7@gmail.com>
2023-02-11 18:26:27 -0300

    feat: adiciona seção de serviço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import { Hero } from '@/components/Hero'
+import { Services } from '@/components/Services'
 
 export default function Home() {
-  return <Hero />
+  return (
+    <>
+    <Hero /> 
+    <Services/>
+    </>
+  )
 }
