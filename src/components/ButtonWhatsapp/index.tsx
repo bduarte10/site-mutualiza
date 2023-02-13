759fce6685d640f1dda3dac3ba65fb0538f5abde
Bruno Duarte <duartebruno2208@gmail.com>
2023-02-13 13:57:08 -0300

    feat: add motion on button

--- src/components/ButtonWhatsapp/index.tsx
+++ src/components/ButtonWhatsapp/index.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { motion } from "framer-motion";
 
 export function ButtonWhatsapp() {
   return (
-    <div className={styles.buttonWhatsapp}>
-      <a className={styles.linkWhats} href="#">
-        <Image
-          src="/assets/whatsapp.svg"
-          alt="whatsapp button"
-          width={32}
-          height={32}
-        />
-      </a>
-    </div>
+    <motion.a
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 1, type: "tween", ease: "easeIn" }}
+      className={styles.buttonWhatsapp}
+    >
+      <Image
+        src="/assets/whatsapp.svg"
+        alt="whatsapp button"
+        width={32}
+        height={32}
+      />
+    </motion.a>
   );
 }
