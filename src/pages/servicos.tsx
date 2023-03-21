092275bc4efd6e541eda49d275d653f891126a38
Bruno Duarte <duartebruno2208@gmail.com>
2023-03-21 16:10:44 -0300

    feat: listagem de serviços

--- src/pages/servicos.tsx
+++ src/pages/servicos.tsx
@@ -1,6 +1,8 @@
-import styles from "../styles/servicos.module.css";
-
-
+import styles from '../styles/servicos.module.css';
+import { services } from '@/components/constants';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { ArrowRight } from 'phosphor-react';
 
 export default function Servicos() {
   return (
@@ -9,7 +11,34 @@ export default function Servicos() {
         <h2>Nossos Serviços</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
-
+      <div className='container'>
+        <div className={styles.content_items}>
+          {services.map((service, index) => (
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: index * 0.1, duration: 0.5 }}
+              key={index}
+            >
+              <Link href={service.link}>
+                <div className={styles.card}>
+                  <div className={styles.icon}>
+                    <span>{service.icon}</span>
+                  </div>
+                  <div className={styles.text}>
+                    <h2>{service.title}</h2>
+                    <p>{service.text}</p>
+                  </div>
+                  <div className={styles.link}>
+                    <span className={styles.spanLink}>Saiba mais</span>
+                    <ArrowRight color='var(--primary)' size={32} />
+                  </div>
+                </div>
+              </Link>
+            </motion.div>
+          ))}
+        </div>
+      </div>
     </main>
-  )
+  );
 }
