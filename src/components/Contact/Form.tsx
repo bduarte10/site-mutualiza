52ba4a12b7a05bda26014af34643bfd60898daec
Bruno Duarte <duartebruno2208@gmail.com>
2023-04-10 14:14:04 -0300

    fix: sendMail

--- src/components/Contact/Form.tsx
+++ src/components/Contact/Form.tsx
@@ -2,46 +2,79 @@ import styles from './contact.module.css';
 import { useState } from 'react';
 import axios from 'axios';
 
-
 export function Form() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [message, setMessage] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setIsLoading(true);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
 
-        try {
-            await axios.post('/api/sendEmail', { name, email, phone, message });
-            setName('');
-            setEmail('');
-            setPhone('');
-            setMessage('');
-            alert('Mensagem enviada com sucesso!');
-        } catch (error) {
-            alert('Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente mais tarde.');
-        }
+    try {
+      await axios.post('/api/contact', { name, email, phone, message });
+      setName('');
+      setEmail('');
+      setPhone('');
+      setMessage('');
+      alert('Mensagem enviada com sucesso!');
+    } catch (error) {
+      alert(
+        'Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente mais tarde.'
+      );
+    }
 
-        setIsLoading(false);
-    };
+    setIsLoading(false);
+  };
 
-    return (
-        <form className={styles.content_items_bottom} onSubmit={handleSubmit}>
-            <h2>Fale Conosco!</h2>
+  return (
+    <form
+      className={styles.content_items_bottom}
+      action='/api/sendEmail'
+      onSubmit={handleSubmit}>
+      <h2>Fale Conosco!</h2>
 
-            <input type="text" id="name" name="name" placeholder="Digite seu nome" value={name} onChange={event => setName(event.target.value)} />
+      <input
+        type='text'
+        id='name'
+        name='name'
+        placeholder='Digite seu nome'
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
 
-            <input type="text" id="email" name="e-mail" placeholder="Digite seu e-mail" value={email} onChange={event => setEmail(event.target.value)} />
+      <input
+        type='text'
+        id='email'
+        name='e-mail'
+        placeholder='Digite seu e-mail'
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
 
-            <input type="number" id="phone" name="phone" placeholder="Digite seu número" value={phone} onChange={event => setPhone(event.target.value)} />
+      <input
+        type='number'
+        id='phone'
+        name='phone'
+        placeholder='Digite seu número'
+        value={phone}
+        onChange={(event) => setPhone(event.target.value)}
+      />
 
-            <label>Conte-nos a sua necessidade</label>
-            <textarea name="" id="" cols={20} rows={10} value={message} onChange={event => setMessage(event.target.value)}></textarea>
+      <label>Conte-nos a sua necessidade</label>
+      <textarea
+        name=''
+        id=''
+        cols={20}
+        rows={10}
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}></textarea>
 
-            <button type="submit" disabled={isLoading}>Enviar</button>
-        </form>
-    );
+      <button type='submit' disabled={isLoading}>
+        Enviar
+      </button>
+    </form>
+  );
 }
--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,35 +1,36 @@
 import nodemailer from 'nodemailer';
-
-
 type ContactFormData = {
-    name: string;
-    email: string;
-    phone: string;
-    message: string;
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
 };
 
-export const sendEmail = async ({ name, email, phone, message }: ContactFormData) => {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.dreamhost.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'contato@mutualiza.com.br',
-            pass: 'ACY66c6P'
-        }
-    });
-    const mailOptions = {
-        from: email,
-        to: 'contato@mutualiza.com.br',
-        subject: 'New message from website',
-        html: `
+export const sendEmail = async ({
+  name,
+  email,
+  phone,
+  message,
+}: ContactFormData) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.dreamhost.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'contato@mutualiza.com.br',
+      pass: 'ACY66c6P',
+    },
+  });
+  const mailOptions = {
+    from: email,
+    to: 'contato@mutualiza.com.br',
+    subject: 'New message from website',
+    html: `
             <p>Name: ${name}</p>
             <p>Email: ${email}</p>
             <p>Phone: ${phone}</p>
             <p>Message: ${message}</p>
-        `
-    };
-    await transporter.sendMail(mailOptions);
+        `,
+  };
+  await transporter.sendMail(mailOptions);
 };
-
-
