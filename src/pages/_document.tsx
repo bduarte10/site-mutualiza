f62cf1f750bade3efba1b047b7a698b9ecdb56b9
Bruno Duarte <duartebruno2208@gmail.com>
2023-04-13 10:30:01 -0300

    feat: tag google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="pt-br">
+    <Html lang='pt-br'>
       <Head>
-        <link rel="shortcut icon" href="/assets/fiveicon.png" type="image/x-icon" />
+        <meta
+          name='google-site-verification'
+          content='mqySQ3hZrPHdozfYscI1SCT52YN5rfCGb_gJr2Sp_O4'
+        />
+        <link
+          rel='shortcut icon'
+          href='/assets/fiveicon.png'
+          type='image/x-icon'
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
