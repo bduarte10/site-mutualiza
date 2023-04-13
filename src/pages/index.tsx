0af8cf68e87d6fe47b16127c88c50bc51d7b47ab
Bruno Duarte <duartebruno2208@gmail.com>
2023-04-13 15:32:51 -0300

    feat: title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { About } from "@/components/About";
-import { Hero } from "@/components/Hero";
-import { Services } from "@/components/Services";
-
+import { About } from '@/components/About';
+import { Hero } from '@/components/Hero';
+import { Services } from '@/components/Services';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Mutualiza - Engenharia e Consultoria Ambiental</title>
+      </Head>
       <Hero />
       <About />
       <Services />
