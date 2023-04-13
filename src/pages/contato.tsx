881f22a4baa12b3df182e3df0d8c2296c236e906
Bruno Duarte <duartebruno2208@gmail.com>
2023-04-13 15:32:38 -0300

    feat: title

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -1,21 +1,23 @@
-import { Contact } from "@/components/Contact/index";
-import styles from "../styles/contato.module.css";
-import { motion } from "framer-motion";
+import { Contact } from '@/components/Contact/index';
+import styles from '../styles/contato.module.css';
+import { motion } from 'framer-motion';
+import Head from 'next/head';
 
 export default function Contato() {
   return (
     <main>
+      <Head>
+        <title>Mutualiza - Contato</title>
+      </Head>
       <div className={styles.content_title}>
         <motion.h2
           initial={{ opacity: 0, x: 50 }}
           whileInView={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.3, duration: 0.7 }}
-        >
+          transition={{ delay: 0.3, duration: 0.7 }}>
           Contato
         </motion.h2>
       </div>
       <Contact />
-
     </main>
-  )
+  );
 }
