aa0e977211ce4aa7b04074001c68cad963bbf36b
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-14 02:10:57 -0300

    fix: adiciona tls no transporter

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -20,6 +20,18 @@ export const sendEmail = async ({
       user: 'contato@mutualiza.com.br',
       pass: 'ACY66c6P',
     },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    }
+  });
+  // verify connection configuration
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
   });
   const mailOptions = {
     from: 'nicolas@mutualiza.com.br',
