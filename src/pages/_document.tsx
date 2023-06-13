c10f188ace3e68e32d2ac98307bb55121d5a223b
israelsanttana <israelsantanasantos7@gmail.com>
2023-06-13 18:54:41 -0300

    fix: corrige local do analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
   return (
     <Html lang="pt-br">
       <Head>
         <link rel="shortcut icon" href="/assets/fiveicon.png" type="image/x-icon" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VGX7VX54H5"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-VGX7VX54H5');
+              `,
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+
