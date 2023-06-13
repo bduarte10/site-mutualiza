30f10a936533aa74b80c5f9989c79ac2d264fb4b
Bruno Duarte <duartebruno2208@gmail.com>
2023-06-13 19:05:34 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang='pt-br'>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VGX7VX54H5"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-VGX7VX54H5');
+              `,
+          }} 
+        />
         <meta
           name='google-site-verification'
           content='mqySQ3hZrPHdozfYscI1SCT52YN5rfCGb_gJr2Sp_O4'
